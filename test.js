c7195929e60684882c33953569166d2b39e003cd
duyvd <duy.vd176936@gmail.com>
2021-07-31 12:34:07 +0700

    update1

---
+++ test.js
@@ -0,0 +1,55 @@
+// BEFORE RUNNING:
+// ---------------
+// 1. If not already done, enable the Google Sheets API
+//    and check the quota for your project at
+//    https://console.developers.google.com/apis/api/sheets
+// 2. Install the Node.js client library by running
+//    `npm install googleapis --save`
+
+const {google} = require('googleapis');
+const sheets = google.sheets('v4');
+
+async function main () {
+  const authClient = await authorize();
+  const request = {
+    // The spreadsheet to apply the updates to.
+    spreadsheetId: '1JrGS9xdOybVbpWTMOVK5mF8irfxxMy9r_Z2UNJC_YPQ',  // TODO: Update placeholder value.
+
+    resource: {
+      // A list of updates to apply to the spreadsheet.
+      // Requests will be applied in the order they are specified.
+      // If any request is not valid, no requests will be applied.
+      requests: [],  // TODO: Update placeholder value.
+
+      // TODO: Add desired properties to the request body.
+    },
+
+    auth: authClient,
+  };
+
+  try {
+    const response = (await sheets.spreadsheets.batchUpdate(request)).data;
+    // TODO: Change code below to process the `response` object:
+    console.log(JSON.stringify(response, null, 2));
+  } catch (err) {
+    console.error(err);
+  }
+}
+main();
+
+async function authorize() {
+  // TODO: Change placeholder below to generate authentication credentials. See
+  // https://developers.google.com/sheets/quickstart/nodejs#step_3_set_up_the_sample
+  //
+  // Authorize using one of the following scopes:
+  //   'https://www.googleapis.com/auth/drive'
+  //   'https://www.googleapis.com/auth/drive.file'
+  //   'https://www.googleapis.com/auth/spreadsheets'
+  let authClient = null;
+
+  if (authClient == null) {
+    throw Error('authentication failed');
+  }
+
+  return authClient;
+}
